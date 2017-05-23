cf4e812a3f9a0e0a5e3566a778219c7207706798
Jonathan Strootman <jstroot@cyverse.org>
2017-05-23 14:22:49 -0700

    Close AMQP connection and enhance logging.
    CORE-6745

--- src/clockwork/amqp.clj
+++ src/clockwork/amqp.clj
@@ -8,6 +8,7 @@
             [langohr.consumers :as lc]
             [langohr.exchange :as le]
             [langohr.basic :as lb]
+            [service-logging.thread-context :as tc]
             [cheshire.core :as cheshire]))
 
 (defn- declare-queue
@@ -43,6 +44,10 @@
     (let [timeNow (new java.util.Date)
           connection (rmq/connect {:uri (config/amqp-uri)})
           channel (lch/open connection)]
+      (tc/with-logging-context
+        {:amqp-routing-key routing-key
+         :amqp-message msg}
+        (log/info (format "Publishing AMQP message. routing-key=%s" routing-key)))
       (lb/publish channel
                   (config/exchange-name)
                   routing-key
@@ -50,6 +55,8 @@
                                     :timestamp_ms (.getTime timeNow)})
                   {:content-type "application/json"
                    :timestamp    timeNow})
-      (lch/close channel))
+
+      (lch/close channel)
+      (rmq/close connection))
     (catch Object _
       (log/error (:throwable &throw-context) "Failed to publish message" (cheshire/encode msg)))))
