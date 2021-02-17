14155bfdf683e085d2e1970048ac98f8d33f626b
Sarah Roberts <sarahr@cyverse.org>
2021-02-17 13:37:55 -0700

    CORE-1256: cleaned up project.clj a little more

--- project.clj
+++ project.clj
@@ -11,7 +11,7 @@
   :description "Scheduled jobs for the CyVerse Discovery Environment"
   :url "https://github.com/cyverse-de/clockwork"
   :license {:name "BSD"
-            :url "http://iplantcollaborative.org/sites/default/files/iPLANT-LICENSE.txt"}
+            :url "https://cyverse.org/license"}
   :manifest {"Git-Ref" ~(git-ref)}
   :uberjar-name "clockwork-standalone.jar"
   :dependencies [[org.clojure/clojure "1.10.2"]
@@ -21,18 +21,14 @@
                                 [com.fasterxml.jackson.core/jackson-annotations]
                                 [com.fasterxml.jackson.core/jackson-databind]
                                 [com.fasterxml.jackson.core/jackson-core]]]
-                 [clj-http "2.0.0"]
                  [clj-time "0.4.5"]
                  [clojurewerkz/quartzite "1.0.1"
                    :exclusions [c3p0]]
                  [com.mchange/c3p0 "0.9.5.1"]
                  [com.novemberain/langohr "3.5.1"]
-                 [korma "0.3.0-RC5"
-                  :exclusions [c3p0]]
                  [org.cyverse/clojure-commons "2.8.0"]
                  [org.cyverse/common-cli "2.8.1"]
                  [org.cyverse/event-messages "0.0.1"]
-                 [org.cyverse/kameleon "3.0.0"]
                  [org.cyverse/service-logging "2.8.0"]
                  [me.raynes/fs "1.4.6"]
                  [slingshot "0.10.3"]]
